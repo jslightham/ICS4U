81001b8d42c771e709f0ba052c265e555ad8ea9b
jslightham <31053827+jslightham@users.noreply.github.com>
2019-10-01 09:37:59 -0400

    delete

--- Unit 1/src/com/bayviewglen/ds/IntBST.java
+++ Unit 1/src/com/bayviewglen/ds/IntBST.java
@@ -70,9 +70,55 @@ public class IntBST{
             inOrderTraversal(node.getRightChild());
     }
 
-    public IntBinaryTreeNode deleteThing(int n){
-        return deleteThing(n, root, null);
+
+    public void delete(Integer data) {
+ 
+        delete(this.root, data);
+    }
+ 
+    public IntBinaryTreeNode delete(IntBinaryTreeNode node, Integer data) {
+        // will run if tree is empty, or when data is not found in 
+        if(node == null) 
+            return node;
+        // set node to returned value through recusion
+        if(data < node.getData()) {
+            node.setLeftChild(delete(node.getLeftChild(), data));
+        } else if(data > node.getData()) {
+            node.setRightChild(delete(node.getRightChild(), data));
+        } 
+        else { // else runs when data = node.getData(), one of 4 possibilities
+            // no children, set to null (bottom of tree)
+            if(node.getLeftChild() == null && node.getRightChild() == null) {
+                return null;
+            } 
+            // only right child, set node to child
+            else if(node.getLeftChild() == null) {
+                return node.getRightChild();
+            } 
+            // only left child, set node to child
+            else if(node.getRightChild() == null) {
+                return node.getLeftChild();
+            } 
+            // when two children
+            else {
+                // get minimum value of tree starting at node
+                int min = minValue(node.getRightChild());
+                // set node to minimum child value
+                node.setData(min);
+                // delete smallest child (should be the first case)
+                node.setRightChild(delete(node.getRightChild(), min));
+            }
+        }
+ 
+        return root;
     }
 
+    public Integer minValue(IntBinaryTreeNode node) {
+ 
+        if(node.getLeftChild() != null) {
+            return minValue(node.getLeftChild());
+        }
+        return node.getData();
+    }
     
 }
--- Unit 1/src/com/bayviewglen/ds/TreeDriver.java
+++ Unit 1/src/com/bayviewglen/ds/TreeDriver.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 
 public class TreeDriver {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner in = new Scanner(new File("C:/Users/sligh/Documents/ICS4U/Unit 1/tree.dat"));
+        Scanner in = new Scanner(new File("C:/Users/jmsdesk/Documents/GitHub/ICS4U/Unit 1/tree.dat"));
         IntBST bst = new IntBST();
         while (in.hasNext()){
             bst.addInteger(in.nextInt());
         }
         bst.inOrderTraversal();
-        bst.deleteThing(2);
+        bst.delete(96);
         bst.inOrderTraversal();
         in.close();
     }
